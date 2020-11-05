9e48e545f1a2d01f9e275031aaa32e6f28be3a1b
Jason Lee <jlsync@gmail.com>
2020-11-05 16:15:51 +1100

    remove single arity template handlers

--- lib/prawnto/template_handlers/base.rb
+++ lib/prawnto/template_handlers/base.rb
@@ -2,7 +2,7 @@ module Prawnto
   module TemplateHandlers
     class Base
       
-      def self.call(template)
+      def self.call(template, source = nil)
         "_prawnto_compile_setup;" +
         "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" + 
         "#{template.source}\n" +
--- lib/prawnto/template_handlers/dsl.rb
+++ lib/prawnto/template_handlers/dsl.rb
@@ -2,7 +2,7 @@ module Prawnto
   module TemplateHandlers
     class Dsl < Base
       
-      def self.call(template)
+      def self.call(template, source = nil)
         "_prawnto_compile_setup;" +
         "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" + 
         "pdf.instance_eval do; #{template.source}\nend;" +
